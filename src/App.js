48f041c1a839dd9d54f07c5316ab1e52059fdb99
ashwhi06 <ashe.white06@gmail.com>
2024-06-15 13:33:21 -0400

    generating data for user request & state

--- src/App.js
+++ src/App.js
@@ -1,34 +1,45 @@
-import { useEffect, useState } from 'react'
-import Gallery from './components/Gallery'
-import SearchBar from './components/SearchBar'
+import { useEffect, useState } from "react";
+import Gallery from "./components/Gallery";
+import SearchBar from "./components/SearchBar";
 
-function App(){
-    let [search, setSearch] = useState('')
-    let [message, setMessage] = useState('Search for Music!')
-    let [data, setData] = useState([])
+function App() {
+  let [search, setSearch] = useState("");
+  let [message, setMessage] = useState("Search for Music!");
+  let [data, setData] = useState([]);
 
+  const API_URL = "https://itunes.apple.com/search?term=";
 
-    useEffect(() => { // This is the first function that runs when the component loads.
-        const fetchData = async() => {
-          document.title = `${search} Music`
-          const response = await fetch('https://itunes.apple.com/search?term=the%20grateful%20dead')
-          const resData = await response.json()
-          if(resData.results.length > 0){
-            setData(resData.results)
-          } else {
-            setData('Not Found')
-          }
+  useEffect(() => {
+    // This is the first function that runs when the component loads.
+    if (search === "") {
+      // If the search term is empty, set the message to 'Search for Music!'.
+      const fetchData = async () => {
+        document.title = `${search} Music`;
+        const response = await fetch("API_URL + search");
+        const resData = await response.json();
+        if (resData.results.length > 0) {
+          setData(resData.results);
+        } else {
+          setData("Not Found");
+        }
+      };
+      fetchData();
     }
-    fetchData()
-  }, [search])
+  }, [search]);
 
-    return (
-        <div>
-            <SearchBar />
-            {message}
-            <Gallery />
-        </div>
-    )
+  const handleSearch = (e, term) => {
+    // This function runs when the user submits the search form.
+    e.preventDefault(); // Prevents the page from reloading.
+    setSearch(term); // Sets the search term to the value of the input field.
+  };
+
+  return (
+    <div>
+      <SearchBar handleSearch={handleSearch} />
+      {message}
+      <Gallery data={data} />
+    </div>
+  );
 }
 
-export default App
+export default App;
